059804bab7de23b645bc11d73b3111db3c053d7e
Aaron Lifton <theflubba@gmail.com>
2013-10-30 17:14:52 -0700

    clojure wtf

--- bin/myproject/core.clj
+++ bin/myproject/core.clj
@@ -12,22 +12,37 @@
     acc
     (recur (dec i) (* acc i)))))
 
-(defn factorial 
+(defn loop-fact [x]
+  (loop [n x f 1]
+    (if (= n 1) f
+      (recur (dec n) (* f n)))))
+    
+
+(defn factorials []
+    (letfn [(factorial-seq [n fact]
+       (lazy-seq
+         (cons fact (factorial-seq (inc n) (* (inc n) fact)))))]
+      (factorial-seq 1 1)))
+
+(def facts (lazy-cat [1] (map * facts (iterate inc 2))))
+(def facts2 (reductions * (iterate inc 1))) 
+
+(defn recur-fact 
     ([n] 
         (factorial n 1)) 
     ([n acc] 
         (if  (= n 0)   acc 
              (recur (dec n) (* acc n)))))
 
-(def funs2 {:loop-test 7 :factorial 4})
-(def funs '{loop-test 7 factorial 4})
+(def funs2 {:loop-fact 5 :recur-fact 4})
+(def funs '{loop-fact 5 recur-fact 4})
 
 ;(doseq [[k v] {"loop-test" 7 "factorial" 4}]
 ;  (prn k v))
 
 (map #((resolve (symbol %1)) %2) ["loop-test" "factorial"] [1 2])  
 
-(doseq [[k,v] {"loop-test" 7 "factorial" 4}]
+(doseq [[k,v] {"loop-test" 4 "factorial" 4}]
   (prn (#((resolve (symbol k)) v))))
 
 ;(apply (loop-test)
